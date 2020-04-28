377e8f21ee75c5d40ed1c77a245aab5ef405bf3e
irfannurahman08 <irfannurahman082gmail.com>
2020-04-28 18:53:29 +0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace P4_Sister_0074
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dgv1 = new System.Windows.Forms.DataGridView();
+            this.buttonHapus = new System.Windows.Forms.Button();
+            this.buttonUbah = new System.Windows.Forms.Button();
+            this.buttonTambah = new System.Windows.Forms.Button();
+            this.txtPenerbit = new System.Windows.Forms.TextBox();
+            this.txtJudul = new System.Windows.Forms.TextBox();
+            this.txtCode = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.buttonSimpan = new System.Windows.Forms.Button();
+            this.buttonAmbil = new System.Windows.Forms.Button();
+            this.Koded = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Juduld = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Penerbitd = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID pelanggan";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(110, 31);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(73, 20);
+            this.txtID.TabIndex = 2;
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(110, 62);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(163, 20);
+            this.txtNama.TabIndex = 3;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(470, 62);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(167, 20);
+            this.txtEmail.TabIndex = 7;
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(470, 31);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(167, 20);
+            this.txtNo.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(394, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Email";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(394, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Telepon";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.groupBox1.Controls.Add(this.dgv1);
+            this.groupBox1.Controls.Add(this.buttonHapus);
+            this.groupBox1.Controls.Add(this.buttonUbah);
+            this.groupBox1.Controls.Add(this.buttonTambah);
+            this.groupBox1.Controls.Add(this.txtPenerbit);
+            this.groupBox1.Controls.Add(this.txtJudul);
+            this.groupBox1.Controls.Add(this.txtCode);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Location = new System.Drawing.Point(15, 101);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(622, 336);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Detail Buku";
+            // 
+            // dgv1
+            // 
+            this.dgv1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgv1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgv1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Koded,
+            this.Juduld,
+            this.Penerbitd});
+            this.dgv1.Location = new System.Drawing.Point(6, 171);
+            this.dgv1.Name = "dgv1";
+            this.dgv1.Size = new System.Drawing.Size(610, 159);
+            this.dgv1.TabIndex = 18;
+            this.dgv1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv1_CellContentClick);
+            this.dgv1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
+            // 
+            // buttonHapus
+            // 
+            this.buttonHapus.Location = new System.Drawing.Point(225, 135);
+            this.buttonHapus.Name = "buttonHapus";
+            this.buttonHapus.Size = new System.Drawing.Size(67, 30);
+            this.buttonHapus.TabIndex = 17;
+            this.buttonHapus.Text = "HAPUS";
+            this.buttonHapus.UseVisualStyleBackColor = true;
+            this.buttonHapus.Click += new System.EventHandler(this.buttonHapus_Click);
+            // 
+            // buttonUbah
+            // 
+            this.buttonUbah.Location = new System.Drawing.Point(129, 135);
+            this.buttonUbah.Name = "buttonUbah";
+            this.buttonUbah.Size = new System.Drawing.Size(67, 30);
+            this.buttonUbah.TabIndex = 16;
+            this.buttonUbah.Text = "UBAH";
+            this.buttonUbah.UseVisualStyleBackColor = true;
+            this.buttonUbah.Click += new System.EventHandler(this.buttonUbah_Click);
+            // 
+            // buttonTambah
+            // 
+            this.buttonTambah.Location = new System.Drawing.Point(40, 135);
+            this.buttonTambah.Name = "buttonTambah";
+            this.buttonTambah.Size = new System.Drawing.Size(67, 30);
+            this.buttonTambah.TabIndex = 15;
+            this.buttonTambah.Text = "TAMBAH";
+            this.buttonTambah.UseVisualStyleBackColor = true;
+            this.buttonTambah.Click += new System.EventHandler(this.buttonTambah_Click);
+            // 
+            // txtPenerbit
+            // 
+            this.txtPenerbit.Location = new System.Drawing.Point(95, 97);
+            this.txtPenerbit.Name = "txtPenerbit";
+            this.txtPenerbit.Size = new System.Drawing.Size(167, 20);
+            this.txtPenerbit.TabIndex = 14;
+            // 
+            // txtJudul
+            // 
+            this.txtJudul.Location = new System.Drawing.Point(95, 66);
+            this.txtJudul.Name = "txtJudul";
+            this.txtJudul.Size = new System.Drawing.Size(385, 20);
+            this.txtJudul.TabIndex = 13;
+            // 
+            // txtCode
+            // 
+            this.txtCode.Location = new System.Drawing.Point(95, 35);
+            this.txtCode.Name = "txtCode";
+            this.txtCode.Size = new System.Drawing.Size(111, 20);
+            this.txtCode.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(22, 100);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Penerbit";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(32, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Judul";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(22, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Kode Buku";
+            // 
+            // buttonSimpan
+            // 
+            this.buttonSimpan.Location = new System.Drawing.Point(12, 443);
+            this.buttonSimpan.Name = "buttonSimpan";
+            this.buttonSimpan.Size = new System.Drawing.Size(124, 30);
+            this.buttonSimpan.TabIndex = 19;
+            this.buttonSimpan.Text = "Simpan File ke XML";
+            this.buttonSimpan.UseVisualStyleBackColor = true;
+            this.buttonSimpan.Click += new System.EventHandler(this.buttonSimpan_Click);
+            // 
+            // buttonAmbil
+            // 
+            this.buttonAmbil.Location = new System.Drawing.Point(547, 443);
+            this.buttonAmbil.Name = "buttonAmbil";
+            this.buttonAmbil.Size = new System.Drawing.Size(90, 30);
+            this.buttonAmbil.TabIndex = 20;
+            this.buttonAmbil.Text = "Ambil file XML";
+            this.buttonAmbil.UseVisualStyleBackColor = true;
+            this.buttonAmbil.Click += new System.EventHandler(this.buttonAmbil_Click);
+            // 
+            // Koded
+            // 
+            this.Koded.HeaderText = "Kode Buku";
+            this.Koded.Name = "Koded";
+            // 
+            // Juduld
+            // 
+            this.Juduld.HeaderText = "Judul Buku";
+            this.Juduld.Name = "Juduld";
+            // 
+            // Penerbitd
+            // 
+            this.Penerbitd.HeaderText = "Penerbit";
+            this.Penerbitd.Name = "Penerbitd";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(657, 490);
+            this.Controls.Add(this.buttonAmbil);
+            this.Controls.Add(this.buttonSimpan);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtNo);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Save To XML | Irfan Nurahman 18.11.0074";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dgv1;
+        private System.Windows.Forms.Button buttonHapus;
+        private System.Windows.Forms.Button buttonUbah;
+        private System.Windows.Forms.Button buttonTambah;
+        private System.Windows.Forms.TextBox txtPenerbit;
+        private System.Windows.Forms.TextBox txtJudul;
+        private System.Windows.Forms.TextBox txtCode;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonSimpan;
+        private System.Windows.Forms.Button buttonAmbil;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Koded;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Juduld;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Penerbitd;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P4_Sister_0074
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void bersihbuku()
+        {
+            txtCode.Clear();
+            txtJudul.Clear();
+            txtPenerbit.Clear();
+            txtCode.Focus();
+        }
+
+        void bersihData()
+        {
+            txtID.Clear();
+            txtNama.Clear();
+            txtNo.Clear();
+            txtEmail.Clear();
+            dgv1.Rows.Clear();
+            bersihbuku();
+            txtID.Focus();
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonTambah_Click(object sender, EventArgs e)
+        {
+            int n = dgv1.Rows.Add();
+            dgv1.Rows[n].Cells[0].Value = txtCode.Text;
+            dgv1.Rows[n].Cells[1].Value = txtJudul.Text;
+            dgv1.Rows[n].Cells[2].Value = txtPenerbit.Text;
+            bersihbuku();
+        }
+
+        private void buttonUbah_Click(object sender, EventArgs e)
+        {
+
+            dgv1.SelectedRows[0].Cells[0].Value = txtCode.Text;
+            dgv1.SelectedRows[0].Cells[1].Value = txtJudul.Text;
+            dgv1.SelectedRows[0].Cells[2].Value = txtPenerbit.Text;
+            bersihbuku();
+        }
+
+        private void buttonHapus_Click(object sender, EventArgs e)
+        {
+            dgv1.Rows.RemoveAt(dgv1.SelectedRows[0].Index);
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            txtCode.Text = dgv1.SelectedRows[0].Cells[0].Value.ToString();
+            txtJudul.Text = dgv1.SelectedRows[0].Cells[1].Value.ToString();
+            txtPenerbit.Text = dgv1.SelectedRows[0].Cells[2].Value.ToString();
+        }
+
+        private void buttonSimpan_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable();
+            dt.TableName = "Pengarang";
+            dt.Columns.Add("ID");
+            dt.Columns.Add("Nama");
+            dt.Columns.Add("Telepon");
+            dt.Columns.Add("Email");
+            ds.Tables.Add(dt);
+
+
+            DataTable dt1 = new DataTable();
+            dt1.TableName = "Buku";
+            dt1.Columns.Add("Kode");
+            dt1.Columns.Add("Judul");
+            dt1.Columns.Add("Penerbit");
+            ds.Tables.Add(dt1);
+
+            DataRow row = ds.Tables["Pengarang"].NewRow();
+            row["ID"] = txtID.Text;
+            row["Nama"] = txtNama.Text;
+            row["Telepon"] = txtNo.Text;
+            row["Email"] = txtEmail.Text;
+            ds.Tables["Pengarang"].Rows.Add(row);
+
+            foreach (DataGridViewRow baris in dgv1.Rows)
+            {
+                DataRow row1 = ds.Tables["Buku"].NewRow();
+                row1["Kode"] = baris.Cells[0].Value;
+                row1["Judul"] = baris.Cells[1].Value;
+                row1["Penerbit"] = baris.Cells[2].Value;
+                ds.Tables["Buku"].Rows.Add(row1);
+
+            }
+			
+			//di rubah y slur nang di koe nyimpen katalog.xml 
+            ds.WriteXml("D:\\katalog.xml");
+            MessageBox.Show("Data sudah tersimpan","Info",
+                MessageBoxButtons.OK,MessageBoxIcon.Information);
+            bersihData();
+        }
+
+        private void buttonAmbil_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+			//di rubah y slur nang di koe nyimpen katalog.xml 
+            ds.ReadXml("D:\\katalog.xml");
+            txtID.Text = ds.Tables["Pengarang"].Rows[0][0].ToString();
+            txtNama.Text = ds.Tables["Pengarang"].Rows[0][1].ToString();
+            txtNo.Text = ds.Tables["Pengarang"].Rows[0][2].ToString();
+            txtEmail.Text = ds.Tables["Pengarang"].Rows[0][3].ToString();
+
+            foreach (DataRow item in ds.Tables["Buku"].Rows)
+            {
+                int n = dgv1.Rows.Add();
+                dgv1.Rows[n].Cells[0].Value = item["Kode"].ToString();
+                dgv1.Rows[n].Cells[1].Value = item["Judul"].ToString();
+                dgv1.Rows[n].Cells[2].Value = item["Penerbit"].ToString();
+            }
+
+        }
+
+        private void dgv1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
